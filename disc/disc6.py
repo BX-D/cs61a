900d77a8cbfc637c334777d74328f79ea0a088fb
BX-D <boxuanduan@uchicago.edu>
2024-11-25 13:25:47 -0600

    update

---
+++ disc/disc6.py
@@ -0,0 +1,39 @@
+def differences(t):
+    """Yield the differences between adjacent values from iterator t.
+
+    >>> list(differences(iter([5, 2, -100, 103])))
+    [-3, -102, 203]
+    >>> next(differences(iter([39, 100])))
+    61
+    """
+    "*** YOUR CODE HERE ***"
+    prev = next(t)
+    for curr in t:
+        yield curr - prev
+        prev = curr
+        
+def partition_gen(n, m):
+    """Yield the partitions of n using parts up to size m.
+
+    >>> for partition in sorted(partition_gen(6, 4)):
+    ...     print(partition)
+    1 + 1 + 1 + 1 + 1 + 1
+    1 + 1 + 1 + 1 + 2
+    1 + 1 + 1 + 3
+    1 + 1 + 2 + 2
+    1 + 1 + 4
+    1 + 2 + 3
+    2 + 2 + 2
+    2 + 4
+    3 + 3
+    """
+    assert n > 0 and m > 0
+    if n == m:
+        yield partition_gen(n, m - 1)
+    if n - m > 0:
+        yield partition_gen(n - m, m)
+    if m > 1:
+        yield partition_gen(n, m - 1)
+
+
+
